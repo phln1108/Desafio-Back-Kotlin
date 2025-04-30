5bf057ea8344ee45e30403a816ca51064fbf3ea5
lucasmelo <lucas-melo59@hotmail.com>
2025-04-29 22:22:02 -0300

    initial commit

--- src/native-test/java/br/com/stellar/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package br.com.stellar;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
